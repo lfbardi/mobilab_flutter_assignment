f8b7902c352d25242521e3eac613f584783e15d2
Luis Felipe Bardi <luisfelipe@lalist.com.br>
2023-08-05 15:23:33 -0300

    added fonts, colors and text styles

---
+++ lib/core/design_system/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Colors.white;
+const kPrimaryColor = Color(0xff2EC4B6);
+const kTitleColor = Colors.black;
+const kDescriptionColor = Color(0xffCCCCCC);
---
+++ lib/core/design_system/text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mobilab_flutter_assignment/core/design_system/colors.dart';
+
+const kTitle = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w500,
+  fontSize: 30,
+  color: Colors.black,
+);
+
+const kDescription = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w400,
+  fontSize: 20,
+  color: kDescriptionColor,
+);
