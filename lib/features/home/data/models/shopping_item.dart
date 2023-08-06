79657c5c2073e025713e31a6ce76b2c3a1f861bd
Luis Felipe Bardi <luisfelipe@lalist.com.br>
2023-08-06 13:11:27 -0300

    models refactor

--- lib/features/home/data/models/shopping_item.dart
+++ lib/features/home/data/models/shopping_item.dart
@@ -1,17 +1,14 @@
 class ShoppingItem {
-  final String id;
   final String title;
   final bool isChecked;
 
   ShoppingItem({
-    required this.id,
     required this.title,
     required this.isChecked,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'id': id,
       'title': title,
       'isChecked': isChecked,
     };
@@ -19,7 +16,6 @@ class ShoppingItem {
 
   factory ShoppingItem.fromMap(Map<String, dynamic> map) {
     return ShoppingItem(
-      id: map['id'] as String,
       title: map['title'] as String,
       isChecked: map['isChecked'] as bool,
     );
--- lib/features/home/data/models/shopping_list.dart
+++ lib/features/home/data/models/shopping_list.dart
@@ -1,7 +1,7 @@
 import 'shopping_item.dart';
 
 class ShoppingList {
-  final String id;
+  final String? id;
   final String title;
   final List<ShoppingItem> items;
 
@@ -13,21 +13,16 @@ class ShoppingList {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'id': id,
       'title': title,
       'items': items.map((x) => x.toMap()).toList(),
     };
   }
 
-  factory ShoppingList.fromMap(Map<String, dynamic> map) {
+  factory ShoppingList.fromMap(String id, Map<String, dynamic> map) {
     return ShoppingList(
-      id: map['id'] as String,
-      title: map['title'] as String,
-      items: List<ShoppingItem>.from(
-        (map['items'] as List<int>).map<ShoppingItem>(
-          (x) => ShoppingItem.fromMap(x as Map<String, dynamic>),
-        ),
-      ),
-    );
+        id: id,
+        title: map['title'] as String,
+        items: List<ShoppingItem>.from(
+            (map['items'] as List).map((x) => ShoppingItem.fromMap(x))));
   }
 }
